332fd7c9b1ccdd8953858c7c9aac8a1be76d2b99
semarmehdi <semarmehdi1@gmail.com>
2025-03-04 14:44:20 +0100

    new app

---
+++ tests/upload_s3.py
@@ -0,0 +1,18 @@
+import boto3
+
+# Initialisation du client S3
+s3_client = boto3.client("s3")
+
+# Définition des paramètres
+file_name = "unit-tests.xml"  # Nom du fichier XML généré par pytest
+bucket_name = "ton-bucket-s3"
+s3_key = "test-results/unit-tests.xml"  # Chemin dans le bucket
+
+# Upload du fichier
+try:
+    s3_client.upload_file(file_name, bucket_name, s3_key)
+    print(
+        f"✅ Fichier '{file_name}' envoyé avec succès sur 's3://{bucket_name}/{s3_key}'"
+    )
+except Exception as e:
+    print(f"❌ Erreur lors de l'upload sur S3 : {e}")
